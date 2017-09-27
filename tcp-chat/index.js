e30991f1f58627346ace964759c23420c2e0eb76
Ian <yt2015.97@gmail.com>
2017-09-27 15:21:39 +0800

    完成

--- tcp-chat/index.js
+++ tcp-chat/index.js
@@ -1,22 +1,46 @@
 const net  = require('net')
 
-let count = 0
+let count = 0, users = {}
 
 const server = net.createServer((conn) => {
 
     conn.setEncoding('utf8')
     conn.write(
-        `
-        > welcome to node-chat!
-        > ${count} other poeple are connected at this time.
-        > please write your name and press enter: `
+        `\n> welcome to node-chat! \n> ${count} other poeple are connected at this time. \n> please write your name and press enter: `
     )
 
     count++
 
     conn.on('close', ()=> count--)
 
-    conn.on('data', (data) => console.log(data))
+    let nickname
+
+    conn.on('data', (data) => {
+
+        data = data.replace('\r\n', '')
+        
+        if(!nickname){
+
+            if(users[data]){
+                conn.write('\033[93m> nickname has already in use. try again: \033[39m')
+                return
+            }else{
+                nickname = data
+                users[nickname] = conn
+                for (let i in users){
+                    users[i].write(`> ${nickname} joined the room \n`)
+                }
+            }
+        } else {
+
+            for(let i in users){
+
+                if(i !== nickname) {
+                    users[i].write(`> ${nickname} : ${data} \n`)
+                }
+            }
+        }
+    })
 })
 
 server.listen(3000, () => {
