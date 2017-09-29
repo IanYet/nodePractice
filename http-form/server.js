d2185ce376db9b8f79cd9b69755bfdeeaccaabd9
Ian <yt2015.97@gmail.com>
2017-09-29 16:38:58 +0800

    finish 404

--- http-form/server.js
+++ http-form/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const qs = require('querystring')
 
 http.createServer((req, res) => {
 
@@ -10,8 +11,24 @@ http.createServer((req, res) => {
         <input type = "text" name = "name">
         <input type = "submit" value = "OKAY">
         </form>`)
-    }else if('/url' === req.url){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end(`you send a ${req.method} request`)
+    }else if('/url' === req.url && 'POST' === req.method){
+        let body = ''
+
+        req.on('data', (chunk) => {
+            body += chunk
+
+            console.log(chunk.toString('utf-8')+'\n')
+        })
+
+        req.on('end', ()=>{
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            // res.end(`<P>Content-Type: ${req.headers['content-type']}</P>
+            // <p>Data: ${body}</p>`)
+
+            res.end(`your name is ${qs.parse(body).name}`)
+        })
+    }else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end(`<p>Oops, you page cannot find</p>`)
     }
 }).listen(3000)
