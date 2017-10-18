8b0e97dd371715144a22d1806ba98b80e960a165
Ian <yt2015.97@gmail.com>
2017-10-18 11:46:21 +0800

    pause

--- connect-website/conServer.js
+++ connect-website/conServer.js
@@ -1,3 +1,10 @@
 const connect = require('connect')
+const static = require('connect-static')
 
 const app = connect()
+const option = {}
+
+static(option, function(err, middleware) {
+    // if (err) throw err;
+    app.use('/', middleware);
+  });
