c8dd4974271587b94e0aceaae98a86d01c34103f
Ian Ye <Ian Ye>
2017-11-01 17:38:30 +0800

    virtual path

--- expressApp/static.js
+++ expressApp/static.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 //url leaf path must be a file instead of folder
-app.use(express.static('public'))
+app.use('/static',express.static('public'))
 
-const server = app.listen(3000, () => {})
+const server = app.listen(3000, () => console.log('connect'))
