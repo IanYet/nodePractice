07cd5e0cee7c2af0b2c76df8c43d73e0b075f3c1
Ian Ye <yt2015.97@gmail.com>
2017-11-14 15:39:47 +0800

    temp submit

--- mongodb/index.js
+++ mongodb/index.js
@@ -10,3 +10,15 @@ app.use(session({secret: 'my secret'}))
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+app.get('/', (req, res) => {
+    app.render('index', {authenticated: false})
+})
+
+app.get('/login', (req, res) => {
+    app.render('login')
+})
+
+app.get('/signup', (req, res) => {
+    app.render('signup')
+})
